8b4b975d83bf003e9e4a4357b50c1763f28fb4d6
Ahmetyldrr <60220232+Ahmetyldrr@users.noreply.github.com>
2021-03-17 15:30:55 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'Testing_p2121',
+    packages = ['.'],
+    version = '0.1.0',
+    description = 'Test',
+    author = 'Ahmet Yıldırır',
+    author_email = 'ahmetyildirir1@gmail.com',
+    url = 'https://github.com/Ahmetyldrr/mypackage_test',
+    keywords = ['TAG1', 'TAG2'],
+    classifiers = [],
+)
